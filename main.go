55aa255f3e3cd0ebc8ee0afec10bc11ecc8d4f3b
Haim <adsyandex@zohomail.com>
2025-01-14 02:34:58 +0000

    Исправление конфликта маршрутов и обновление main.go

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "Project_Telegram/quickstart/controllers"
+	"Project_Telegram/quickstart/controllers"
+	"log"
+	"net/http"
 
-    "github.com/astaxie/beego"
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/astaxie/beego"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-    bot, err := tgbotapi.NewBotAPI("5471771233:AAHMkVW2hezXa7xak2gVirWzhBj8XaQ_xh8")
-    if err != nil {
-        log.Panic(err)
-    }
+	bot, err := tgbotapi.NewBotAPI("5471771233:AAHMkVW2hezXa7xak2gVirWzhBj8XaQ_xh8")
+	if err != nil {
+		log.Panic(err)
+	}
 
-    updates := bot.ListenForWebhook("/webhook")
-    http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-        controller := &controllers.WebhookController{Bot: bot, Updates: updates}
-        controller.HandleWebhook(w, r)
-    })
+	updates := bot.ListenForWebhook("/webhook")
+	http.HandleFunc("/telegram_webhook", func(w http.ResponseWriter, r *http.Request) {
+		controller := &controllers.WebhookController{Bot: bot, Updates: updates}
+		controller.HandleWebhook(w, r)
+	})
 
-    beego.BConfig.Listen.HTTPSCertFile = "path/to/cert.pem"
-    beego.BConfig.Listen.HTTPSKeyFile = "path/to/key.pem"
-    beego.BConfig.Listen.HTTPPort = 443
-    beego.BConfig.Listen.EnableHTTP = false
-    beego.BConfig.Listen.EnableHTTPS = true
+	beego.BConfig.Listen.HTTPSCertFile = "path/to/cert.pem"
+	beego.BConfig.Listen.HTTPSKeyFile = "path/to/key.pem"
+	beego.BConfig.Listen.HTTPPort = 443
+	beego.BConfig.Listen.EnableHTTP = false
+	beego.BConfig.Listen.EnableHTTPS = true
 
-    log.Println("Запуск сервера...")
-    beego.Run()
+	log.Println("Запуск сервера...")
+	beego.Run()
 }
